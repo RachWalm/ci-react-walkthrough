4e8d81395fafd864fd12732348b222c1ae92213a
Rachel Walmsley <rachwalm75@gmail.com>
2024-03-01 16:56:10 +0000

    Fix axios url pointing from front end to front end -  backend

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://ci-react-walkthrough-5407e02d9c4b.herokuapp.com/'
+axios.defaults.baseURL = 'https://profile-app-165689e2f46b.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
