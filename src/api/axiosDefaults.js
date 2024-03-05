d0527d42ee80f2b8430e2bc61ec44e1e5374e512
Rachel Walmsley <rachwalm75@gmail.com>
2024-03-05 12:26:03 +0000

    Add navbar icons and finish sign in/signout

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://profile-app-165689e2f46b.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
