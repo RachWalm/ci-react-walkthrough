b6cae9609143e7ab7dab1894d40a498323ea85c3
Rachel Walmsley <rachwalm75@gmail.com>
2024-03-26 09:34:11 +0000

    Add Edit function to comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Media } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Avatar from '../../components/Avatar';
 import styles from '../../styles/Comment.module.css';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from "./CommentEditForm";
+const [showEditForm, setShowEditForm] = useState(false);
+
 
 const Comment = (props) => {
     const {
@@ -10,28 +16,64 @@ const Comment = (props) => {
         profile_image,
         owner,
         updated_at,
-        content
+        content,
+        id,
+        setPost,
+        setComments,
     } = props;
 
-  return (
-    <div>
-      <hr />
-      <Media>
-        <Link to={`/profile/${profile_id}`}>
-            <Avatar src={profile_image} />
-        </Link>
-        <Media.Body className="align-self-center ml-2">
-            <span className={styles.Owner}>
-                {owner}
-            </span>
-            <span className={styles.Date}>
-                {updated_at}
-            </span>
-            <p>{content}</p>
-        </Media.Body>
-      </Media>
-    </div>
-  )
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+    const history = useHistory();
+
+    const handleDelete = async () => {
+        try{
+            await axiosRes.delete(`/comments/${id}/`);
+            setPost(prevPost => ({
+                ...prevPost.restuls[0],
+                comments_count: prevPost.results[0].comments_count - 1
+            }));
+
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.filter ((comment) => comment.id !== id),
+            }));
+        } catch(err) {
+
+        }
+    }
+    return (
+        <>
+          <hr />
+          <Media>
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profile_image} />
+            </Link>
+            <Media.Body className="align-self-center ml-2">
+              <span className={styles.Owner}>{owner}</span>
+              <span className={styles.Date}>{updated_at}</span>
+              {showEditForm ? (
+                <CommentEditForm
+                id={id}
+                profile_id={profile_id}
+                content={content}
+                profileImage={profile_image}
+                setComments={setComments}
+                setShowEditForm={setShowEditForm}
+              />
+              ) : (
+                <p>{content}</p>
+              )}
+            </Media.Body>
+            {is_owner && !showEditForm && (
+              <MoreDropdown
+                handleEdit={() => setShowEditForm(true)}
+                handleDelete={handleDelete}
+              />
+            )}
+          </Media>
+        </>
+      );
 }
 
 export default Comment
