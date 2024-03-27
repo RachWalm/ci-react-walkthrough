75fe35e774cbc14d1539cff481a977cccb7f80b6
Rachel Walmsley <rachwalm75@gmail.com>
2024-03-27 15:58:27 +0000

    Fix errors in spacing on PopularProfiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -9,19 +9,22 @@ const PopularProfiles = ({ mobile }) => {
   const { popularProfiles } = useProfileData();
 
   return (
-    <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'
-    }`}>
+    <Container className={`${appStyles.Content} ${
+      mobile && "d-lg-none text-center mb-3"
+    }`}
+    >
       {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
-            <Profile key= {profile.id} profile={profile} mobile />
+            <Profile key={profile.id} profile={profile} mobile />
           ))}
             </div>
-            ) : (popularProfiles.results.map((profile) => (
-              <Profile key= {profile.id} profile={profile} />
+            ) : (
+              popularProfiles.results.map((profile) => (
+              <Profile key={profile.id} profile={profile} />
             ))
             )}
         </>
