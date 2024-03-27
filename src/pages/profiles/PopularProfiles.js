b897d19c6305e91744edfbab98fe9218d6704d1b
Rachel Walmsley <rachwalm75@gmail.com>
2024-03-27 07:41:52 +0000

    Feat. Popular profiles follow unfollow buttons

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,6 +4,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Profile from "../../pages/profiles/Profile"
 
 const PopularProfiles = ({ mobile }) => {
   const [profileData, setProfileData] = useState({
@@ -41,11 +42,11 @@ const PopularProfiles = ({ mobile }) => {
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
-            <p key={profile.id}>{profile.owner}</p>
+            <Profile key= {profile.id} profile={profile} mobile />
           ))}
             </div>
             ) : (popularProfiles.results.map((profile) => (
-              <p key={profile.id}>{profile.owner}</p>
+              <Profile key= {profile.id} profile={profile} />
             ))
             )}
         </>
